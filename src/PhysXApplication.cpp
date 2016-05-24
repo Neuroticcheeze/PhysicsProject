146c43010ae8461c003f19068e5dcb7eca3293fe
Neuroticcheeze <neuroticcheeze@Hotmail.com>
2016-05-24 22:08:53 +1000

    PhysX now supportted

--- src/PhysXApplication.cpp
+++ src/PhysXApplication.cpp
@@ -12,32 +12,75 @@ using glm::vec3;
 using glm::vec4;
 using glm::mat4;
 
-//PhysX Includes
-#include <PxPhysicsAPI.h>
-using namespace physx;
-// Physx Physics
-PxPhysics *mPhysics = NULL;
-PxFoundation *mFoundation = NULL;
-PxScene * mScene = NULL;
 
-PxRigidActor *shape0;
 
-void initPhysX()
+void PhysXApplication::setUpPhysX()
 {
-	static PxDefaultErrorCallback gDefaultErrorCallback;
-	static PxDefaultAllocator gDefaultAllocatorCallback;
-
-	// create foundation object with default error and allocator callbacks.
-	mFoundation = PxCreateFoundation(
-		PX_PHYSICS_VERSION,
-		gDefaultAllocatorCallback,
+	PxAllocatorCallback *myCallback = new myAllocator();
+	g_PhysicsFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, *myCallback,
 		gDefaultErrorCallback);
-
-	// create Physics object with the created foundation and with a 'default' scale tolerance.
-	mPhysics = PxCreatePhysics(
-		PX_PHYSICS_VERSION,
-		*mFoundation,
+	g_Physics = PxCreatePhysics(PX_PHYSICS_VERSION, *g_PhysicsFoundation,
 		PxTolerancesScale());
+	PxInitExtensions(*g_Physics);
+	//create physics material 
+	g_PhysicsMaterial = g_Physics->createMaterial(0.5f, 0.5f, .5f);
+	PxSceneDesc sceneDesc(g_Physics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0, -10.0f, 0);
+	sceneDesc.filterShader = &physx::PxDefaultSimulationFilterShader;
+	sceneDesc.cpuDispatcher = PxDefaultCpuDispatcherCreate(1);
+	g_PhysicsScene = g_Physics->createScene(sceneDesc);
+}
+
+void PhysXApplication::upDatePhysX(float a_deltaTime)
+{
+	if (a_deltaTime <= 0)
+	{
+		return;
+	}
+	g_PhysicsScene->simulate(a_deltaTime);
+	while (g_PhysicsScene->fetchResults() == false)
+	{
+		// don’t need to do anything here yet but we have to fetch results
+	}
+}
+
+void PhysXApplication::setUpVisualDebugger()
+{
+	// check if PvdConnection manager is available on this platform
+	if (g_Physics->getPvdConnectionManager() == NULL)
+		return;
+	// setup connection parameters
+	const char* pvd_host_ip = "127.0.0.1";
+	// IP of the PC which is running PVD
+	int port = 5425;
+	// TCP port to connect to, where PVD is listening
+	unsigned int timeout = 100;
+	// timeout in milliseconds to wait for PVD to respond,
+	//consoles and remote PCs need a higher timeout.
+	PxVisualDebuggerConnectionFlags connectionFlags = PxVisualDebuggerExt::getAllConnectionFlags();
+	// and now try to connectPxVisualDebuggerExt
+	auto theConnection = PxVisualDebuggerExt::createConnection(
+		g_Physics->getPvdConnectionManager(), pvd_host_ip, port, timeout, connectionFlags);
+}
+
+void PhysXApplication::setupTutorial1()
+{
+	//add a plane
+	PxTransform pose = PxTransform(PxVec3(0.0f, 0, 0.0f), PxQuat(PxHalfPi*1.0f,
+		PxVec3(0.0f, 0.0f, 1.0f)));
+	PxRigidStatic* plane = PxCreateStatic(*g_Physics, pose, PxPlaneGeometry(),
+		*g_PhysicsMaterial);
+	//add it to the physX scene
+	g_PhysicsScene->addActor(*plane);
+
+	//add a box
+	float density = 10;
+	PxBoxGeometry box(2, 2, 2);
+	PxTransform transform(PxVec3(0, 20, 0));
+	PxRigidDynamic* dynamicActor = PxCreateDynamic(*g_Physics, transform, box,
+		*g_PhysicsMaterial, density);
+	//add it to the physX scene
+	g_PhysicsScene->addActor(*dynamicActor);
 }
 
 PhysXApplication::PhysXApplication()
@@ -65,7 +108,9 @@ bool PhysXApplication::startup()
 	//////////////////////////////////////////////////////////////////////////
 	// YOUR STARTUP CODE HERE
 	//////////////////////////////////////////////////////////////////////////
-	initPhysX();
+	setUpPhysX();
+	setUpVisualDebugger();
+	setupTutorial1();
 
 	return true;
 }
@@ -75,8 +120,9 @@ void PhysXApplication::shutdown() {
 	//////////////////////////////////////////////////////////////////////////
 	// YOUR SHUTDOWN CODE HERE
 	//////////////////////////////////////////////////////////////////////////
-	mPhysics->release();
-	mFoundation->release();
+	g_PhysicsScene->release();
+	g_Physics->release();
+	g_PhysicsFoundation->release();
 
 	// delete our camera and cleanup gizmos
 	delete m_camera;
@@ -102,6 +148,7 @@ bool PhysXApplication::update(float deltaTime)
 	//////////////////////////////////////////////////////////////////////////
 	// YOUR UPDATE CODE HERE
 	//////////////////////////////////////////////////////////////////////////
+	upDatePhysX(deltaTime);
 
 	// return true, else the application closes
 	return true;
--- src/PhysXApplication.h
+++ src/PhysXApplication.h
@@ -5,15 +5,39 @@
 // only needed for the camera picking
 #include <glm/vec3.hpp>
 
-#include <vector>
+//PhysX Includes
+#include <PxPhysicsAPI.h>
+#include <PxScene.h>
+#include <pvd/PxVisualDebugger.h>
+using namespace physx;
 
 class Camera;
+
+class myAllocator : public PxAllocatorCallback
+{
+public:
+	virtual ~myAllocator() {}
+	virtual void* allocate(size_t size, const char* typeName, const char* filename, int line)
+	{
+		void* pointer = _aligned_malloc(size, 16);
+		return pointer;
+	}
+	virtual void deallocate(void* ptr)
+	{
+		_aligned_free(ptr);
+	}
+};
 
 class PhysXApplication : public BaseApplication {
 public:
 
 	PhysXApplication();
 	virtual ~PhysXApplication();
+
+	void setUpPhysX();
+	void upDatePhysX(float a_deltaTime);
+	void setUpVisualDebugger();
+	void setupTutorial1();
 
 	virtual bool startup();
 	virtual void shutdown();
@@ -22,6 +46,16 @@ public:
 	virtual void draw();
 
 private:
+
+	PxFoundation* g_PhysicsFoundation;
+	PxPhysics* g_Physics;
+	PxScene* g_PhysicsScene;
+	PxDefaultErrorCallback gDefaultErrorCallback;
+	PxDefaultAllocator gDefaultAllocatorCallback;
+	PxSimulationFilterShader gDefaultFilterShader = PxDefaultSimulationFilterShader;
+	PxMaterial* g_PhysicsMaterial;
+	PxMaterial* g_boxMaterial;
+	PxCooking* g_PhysicsCooker;
 
 	Camera*		m_camera;
 };
